a97e0607956d18e0112a0bb438b121e98b285e59
connectedwizzard <connwizzard@protonmail.com>
2023-03-25 01:23:15 +0100

    Updated

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,57 +5,54 @@ function is_installed {
     dpkg -s $1 &> /dev/null
 }
 
+# List of applications and their installation commands
+applications=(
+    ["VSCodium"]="wget -qO- https://git.io/vscodium-install | bash"
+    ["GIMP"]="sudo apt-get install gimp -y"
+    ["Apache"]="sudo apt-get install apache2 -y"
+    ["VirtualBox"]="sudo apt-get install virtualbox -y"
+    ["KeePassXC"]="sudo apt-get install keepassxc -y"
+    ["Firefox"]="sudo apt-get install firefox -y"
+    ["VLC"]="sudo apt-get install vlc -y"
+    ["Thunderbird"]="sudo apt-get install thunderbird -y"
+    ["Inkscape"]="sudo apt-get install inkscape -y"
+    ["Blender"]="sudo apt-get install blender -y"
+    ["Krita"]="sudo apt-get install krita -y"
+    ["Git"]="sudo apt-get install git -y"
+    ["Wine"]="sudo apt-get install wine-stable -y"
+    ["OBS Studio"]="sudo apt-get install obs-studio -y"
+    ["Audacity"]="sudo apt-get install audacity -y"
+    ["Shotcut"]="sudo apt-get install shotcut -y"
+)
+
 # Main menu
 echo "Please select an option:"
-echo "[1] Install VSCodium"
-echo "[2] Install GIMP"
-echo "[3] Install Apache"
-echo "[4] Update System"
+for i in "${!applications[@]}"; do
+    echo "[$((i+1))] Install ${i}"
+done
+echo "[$((i+2))] Update System"
 read option
 
-case $option in
-    1)
-        # Check if VSCodium is already installed
-        if is_installed codium; then
-            echo "VSCodium is already installed."
-        else
-            # Install VSCodium
-            echo "Installing VSCodium..."
-            wget -qO- https://git.io/vscodium-install | bash
-            echo "VSCodium has been installed."
-        fi
-        ;;
-    2)
-        # Check if GIMP is already installed
-        if is_installed gimp; then
-            echo "GIMP is already installed."
-        else
-            # Install GIMP
-            echo "Installing GIMP..."
-            sudo apt-get install gimp -y
-            echo "GIMP has been installed."
-        fi
-        ;;
-    3)
-        # Check if Apache is already installed
-        if is_installed apache2; then
-            echo "Apache is already installed."
-        else
-            # Install Apache
-            echo "Installing Apache..."
-            sudo apt-get install apache2 -y
-            echo "Apache has been installed."
-        fi
-        ;;
-    4)
-        # Update the system
-        echo "Updating system..."
-        sudo apt-get update
-        sudo apt-get upgrade -y
-        echo "System has been updated."
-        ;;
-    *)
-        # Invalid option
-        echo "Invalid option."
-        ;;
-esac
+# Install or update based on user's choice
+if ((option >= 1 && option <= ${#applications[@]})); then
+    app_name="${!applications[option-1]}"
+    app_cmd="${applications[$app_name]}"
+    # Check if the application is already installed
+    if is_installed $app_name; then
+        echo "$app_name is already installed."
+    else
+        # Install the application
+        echo "Installing $app_name..."
+        eval "$app_cmd"
+        echo "$app_name has been installed."
+    fi
+elif ((option == ${#applications[@]}+1)); then
+    # Update the system
+    echo "Updating system..."
+    sudo apt-get update
+    sudo apt-get upgrade -y
+    echo "System has been updated."
+else
+    # Invalid option
+    echo "Invalid option."
+fi
